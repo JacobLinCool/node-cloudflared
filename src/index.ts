c96ab4e23b1beb210f81c11716880eb5fff1ea97
JacobLinCool <jacoblincool@gmail.com>
2022-07-29 03:32:50 +0800

    feat: `bin` subcommand for binary management

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,47 @@ import { bin, install } from "./lib.js";
 main();
 
 async function main() {
-    if (!fs.existsSync(bin)) {
-        console.log("Installed cloudflared to " + (await install(bin)));
-    }
-
     const args = process.argv.slice(2);
 
-    if (args[0] === "remove-bin") {
-        fs.unlinkSync(bin);
-        console.log("Removed cloudflared");
-        process.exit(0);
+    if (args[0] === "bin") {
+        if (!args[1]) {
+            console.log(bin);
+            return;
+        }
+        if (args[1] === "remove") {
+            fs.unlinkSync(bin);
+            console.log("Removed cloudflared");
+            return;
+        }
+        if (args[1] === "install") {
+            if (args[2]) {
+                console.log(`Installing cloudflared ${args[2]}`);
+                console.log(await install(bin, args[2]));
+            } else {
+                console.log("Installing latest version of cloudflared");
+                await install(bin);
+            }
+            return;
+        }
+        if (args[1] === "help" || args[1] === "--help" || args[1] === "-h") {
+            console.log(`cloudflared bin                    : Prints the path to the binary`);
+            console.log(`cloudflared bin remove             : Removes the binary`);
+            console.log(`cloudflared bin install [version]  : Installs the binary`);
+            console.log(`cloudflared bin help               : Prints this help message`);
+            console.log(`Examples:`);
+            console.log(
+                `cloudflared bin install            : Installs the latest version of cloudflared`,
+            );
+            console.log(`cloudflared bin install 2022.7.1   : Installs cloudflared 2022.7.1`);
+            console.log(
+                `You can find releases at https://github.com/cloudflare/cloudflared/releases`,
+            );
+            return;
+        }
+    }
+
+    if (!fs.existsSync(bin)) {
+        console.log("Installed cloudflared to " + (await install(bin)));
     }
 
     const sub = spawn(bin, args, { shell: true, stdio: "inherit" });
