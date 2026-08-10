f5ea4ce078ee1ce23f68c5f7e7db3413544b38eb
JacobLinCool <JacobLinCool@users.noreply.github.com>
2026-08-10 22:42:56 +0000

    deploy: ae5a6666c97a5d37c35ee06a929bf246c8fbb58f

---
+++ media/tunnel.mjs
@@ -0,0 +1,29 @@
+import fs from "node:fs";
+import { Tunnel, bin, install } from "cloudflared";
+
+console.log("Cloudflared Tunnel Example.");
+main();
+
+async function main() {
+    if (!fs.existsSync(bin)) {
+        // install cloudflared binary
+        await install(bin)
+    }
+
+    // run: cloudflared tunnel --hello-world
+    const tunnel = Tunnel.quick();
+
+    // show the url
+    const url = new Promise((resolve) => tunnel.once("url", resolve));
+    console.log("LINK:", await url);
+
+    const conn = new Promise((resolve) => tunnel.once("connected", resolve));
+    console.log("CONN:", await conn);
+
+    // stop the tunnel after 15 seconds
+    setTimeout(tunnel.stop, 15_000);
+
+    tunnel.on("exit", (code) => {
+        console.log("tunnel process exited with code", code);
+    });
+}
